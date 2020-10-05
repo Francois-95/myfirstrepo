f1b459c6b09665193911038e17f82ab804e5cee8
Spinks <spinks.code@outlook.com>
2020-10-05 22:07:04 +0200

    My First CPP Program

---
+++ myfirstrepo/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	// Pushes "Hello World" to cout (from std library) - as #included above
+	std::cout << "Hello World" << std::endl;
+	// "Return 0" is the desired outcome. 0=no faults 1=fault - See posix status returns
+	return 0;
+}
